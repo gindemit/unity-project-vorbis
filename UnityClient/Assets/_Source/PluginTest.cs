fa92b7da4a4f71493f329f59200789a51edf7b41
Konstantin Gindemit <k.hindemit@rambler.ru>
2019-10-06 16:12:18 +0200

    + Added source files to test the Plugin

---
+++ UnityClient/Assets/_Source/PluginTest.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class PluginTest : MonoBehaviour
+{
+    [System.Serializable]
+    private struct Data
+    {
+        public float[] PCMData;
+    }
+
+    [SerializeField] private AudioSource _audioSource;
+
+    public void SaveAudioClipToOgg()
+    {
+        float[] pcm = new float[_audioSource.clip.samples];
+        _audioSource.clip.GetData(pcm, 0);
+
+        string dirToSave = Path.Combine(Application.persistentDataPath, "Ogg");
+        Directory.CreateDirectory(dirToSave);
+        string pathToSave = Path.Combine(dirToSave, "1");
+
+        Data data = new Data() { PCMData = pcm };
+
+        File.WriteAllText(pathToSave + ".json", JsonUtility.ToJson(data));
+        Debug.Log(pcm.Length);
+
+        Presentation.Utility.Loader.WaveAudio.Save(pathToSave + ".wav", _audioSource.clip);
+        VorbisPlugin.EncodePcmDataToFile(pcm, pcm.Length, 1, 44100, 0.4f, pathToSave + ".ogg");
+
+        Debug.Log(pathToSave);
+    }
+}
---
+++ UnityClient/Assets/_Source/VorbisPlugin.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+
+public sealed class VorbisPlugin
+{
+
+    [DllImport("VorbisPlugin")]
+    public static extern int EncodePcmDataToFile(float[] samples, int samplesLength, short channels, int frequency, float base_quality, string filePath);
+}
