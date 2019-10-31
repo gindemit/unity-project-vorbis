f284dc494702655c780c452a770491ec8763fe54
Konstantin Gindemit <k.hindemit@rambler.ru>
2019-10-31 21:59:26 +0100

    + Added StreamTest

---
+++ UnityClient/Assets/_Source/StreamTest.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class StreamTest : MonoBehaviour
+{
+    private System.IntPtr _fileStream;
+
+    private void OnEnable()
+    {
+        string workingDirectory = Path.Combine(Application.persistentDataPath, "VorbisPluginTest");
+
+        string pathToFile = Path.Combine(workingDirectory, "vivaldi-summer-the-four-seasons.ogg");
+        _fileStream = VorbisPlugin.OpenReadFileStream(pathToFile, out short channels, out int frequency);
+        Debug.Log($"Successesfully opened filestream, channels: {channels}, frequency: {frequency}");
+    }
+    private void OnDisable()
+    {
+        VorbisPlugin.CloseFileStream(_fileStream);
+        Debug.Log($"Successesfully closed filestream");
+    }
+
+    private void OnAudioFilterRead(float[] data, int channels)
+    {
+        int requestedSamples = data.Length / channels;
+        VorbisPlugin.ReadFromFileStream(_fileStream, data, requestedSamples);
+    }
+}
