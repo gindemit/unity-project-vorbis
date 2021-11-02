21f3e4501e9c12a05f6573929ff1e8474501e75e
Konstantin Gindemit <k.hindemit@rambler.ru>
2021-11-02 22:53:24 +0100

    ~ Implemented converting from byte array to AudioClip method. try .. finally .. for releasing the native memory int pointers.

--- UnityClient/Assets/VorbisPlugin/Impl/src/VorbisPlugin.cs
+++ UnityClient/Assets/VorbisPlugin/Impl/src/VorbisPlugin.cs
@@ -55,23 +55,31 @@ namespace OggVorbis
             {
                 throw new System.ArgumentException($"Only one or two channels are supported, provided channels count: {finalChannelsCount}");
             }
-
-            float[] pcm = new float[audioClip.samples * audioClip.channels];
-            audioClip.GetData(pcm, 0);
-            int returnCode = NativeBridge.WriteAllPcmDataToMemory(
-                out System.IntPtr bytesPtr,
-                out int bytesLength,
-                pcm,
-                pcm.Length,
-                finalChannelsCount,
-                audioClip.frequency,
-                quality,
-                samplesToRead);
-            NativeErrorException.ThrowExceptionIfNecessary(returnCode);
-            byte[] bytes = new byte[bytesLength];
-            Marshal.Copy(bytesPtr, bytes, 0, bytesLength);
-            returnCode = NativeBridge.FreeMemoryArrayForWriteAllPcmData(bytesPtr);
-            NativeErrorException.ThrowExceptionIfNecessary(returnCode);
+            int returnCode;
+            System.IntPtr bytesPtr = System.IntPtr.Zero;
+            byte[] bytes;
+            try
+            {
+                float[] pcm = new float[audioClip.samples * audioClip.channels];
+                audioClip.GetData(pcm, 0);
+                returnCode = NativeBridge.WriteAllPcmDataToMemory(
+                    out bytesPtr,
+                    out int bytesLength,
+                    pcm,
+                    pcm.Length,
+                    finalChannelsCount,
+                    audioClip.frequency,
+                    quality,
+                    samplesToRead);
+                NativeErrorException.ThrowExceptionIfNecessary(returnCode);
+                bytes = new byte[bytesLength];
+                Marshal.Copy(bytesPtr, bytes, 0, bytesLength);
+            }
+            finally
+            {
+                returnCode = NativeBridge.FreeMemoryArrayForWriteAllPcmData(bytesPtr);
+                NativeErrorException.ThrowExceptionIfNecessary(returnCode);
+            }
             return bytes;
         }
 
@@ -89,22 +97,73 @@ namespace OggVorbis
             {
                 throw new System.IO.FileNotFoundException();
             }
-            int returnCode = NativeBridge.ReadAllPcmDataFromFile(
-                filePath,
-                out System.IntPtr pcmPtr,
-                out int pcmLength,
-                out short channels,
-                out int frequency,
-                maxSamplesToRead);
-            NativeErrorException.ThrowExceptionIfNecessary(returnCode);
-            float[] pcm = new float[pcmLength];
-            Marshal.Copy(pcmPtr, pcm, 0, pcmLength);
-            returnCode = NativeBridge.FreeSamplesArrayNativeMemory(ref pcmPtr);
-            NativeErrorException.ThrowExceptionIfNecessary(returnCode);
-
-            var audioClip = UnityEngine.AudioClip.Create(System.IO.Path.GetFileName(filePath), pcmLength / channels, channels, frequency, false);
-            audioClip.SetData(pcm, 0);
-
+            int returnCode;
+            System.IntPtr pcmPtr = System.IntPtr.Zero;
+            UnityEngine.AudioClip audioClip;
+            try
+            {
+                returnCode = NativeBridge.ReadAllPcmDataFromFile(
+                    filePath,
+                    out pcmPtr,
+                    out int pcmLength,
+                    out short channels,
+                    out int frequency,
+                    maxSamplesToRead);
+                NativeErrorException.ThrowExceptionIfNecessary(returnCode);
+                float[] pcm = new float[pcmLength];
+                Marshal.Copy(pcmPtr, pcm, 0, pcmLength);
+                audioClip = UnityEngine.AudioClip.Create(System.IO.Path.GetFileName(filePath), pcmLength / channels, channels, frequency, false);
+                audioClip.SetData(pcm, 0);
+            }
+            finally
+            {
+                returnCode = NativeBridge.FreeSamplesArrayNativeMemory(ref pcmPtr);
+                NativeErrorException.ThrowExceptionIfNecessary(returnCode);
+            }
+            return audioClip;
+        }
+        public static UnityEngine.AudioClip ToAudioClip(byte[] bytes, string audioClipName, int maxSamplesToRead = 1024)
+        {
+            if (bytes == null)
+            {
+                throw new System.ArgumentNullException(nameof(bytes));
+            }
+            if (bytes.Length < 10)
+            {
+                throw new System.ArgumentException(nameof(bytes));
+            }
+            if (string.IsNullOrWhiteSpace(audioClipName))
+            {
+                throw new System.ArgumentException("Please provide an audio clip name");
+            }
+            if (maxSamplesToRead <= 0)
+            {
+                throw new System.ArgumentOutOfRangeException(nameof(maxSamplesToRead));
+            }
+            int returnCode;
+            System.IntPtr pcmPtr = System.IntPtr.Zero;
+            UnityEngine.AudioClip audioClip = null;
+            try
+            {
+                returnCode = NativeBridge.ReadAllPcmDataFromMemory(
+                    bytes,
+                    bytes.Length,
+                    out pcmPtr,
+                    out int pcmLength,
+                    out short channels,
+                    out int frequency,
+                    maxSamplesToRead);
+                NativeErrorException.ThrowExceptionIfNecessary(returnCode);
+                float[] pcm = new float[pcmLength];
+                Marshal.Copy(pcmPtr, pcm, 0, pcmLength);
+                audioClip = UnityEngine.AudioClip.Create(audioClipName, pcmLength / channels, channels, frequency, false);
+                audioClip.SetData(pcm, 0);
+            }
+            finally
+            {
+                returnCode = NativeBridge.FreeSamplesArrayNativeMemory(ref pcmPtr);
+                NativeErrorException.ThrowExceptionIfNecessary(returnCode);
+            }
             return audioClip;
         }
     }
