02ee280461ba1c1fd8f2131c0f8e09908c6eaa58
Konstantin Gindemit <k.hindemit@rambler.ru>
2021-11-02 22:18:24 +0100

    ~ Refactored out all external native method to NativeBridge class

--- UnityClient/Assets/VorbisPlugin/Impl/src/VorbisPlugin.cs
+++ UnityClient/Assets/VorbisPlugin/Impl/src/VorbisPlugin.cs
@@ -4,89 +4,6 @@ namespace OggVorbis
 {
     public static class VorbisPlugin
     {
-#if UNITY_IOS && !UNITY_EDITOR
-    private const string PLUGIN_NAME = "__Internal";
-#else
-        private const string PLUGIN_NAME = "VorbisPlugin";
-#endif
-
-        [DllImport(PLUGIN_NAME,
-            CallingConvention = CallingConvention.Cdecl,
-            EntryPoint = "write_all_pcm_data_to_file")]
-        private static extern int WriteAllPcmDataToFile(
-            string filePath,
-            float[] samples,
-            int samplesLength,
-            short channels,
-            int frequency,
-            float base_quality,
-            int samplesToRead);
-        [DllImport(PLUGIN_NAME,
-            CallingConvention = CallingConvention.Cdecl,
-            EntryPoint = "write_all_pcm_data_to_memory")]
-        private static extern int WriteAllPcmDataToMemory(
-            out System.IntPtr bytesPtr,
-            out int bytesLength,
-            float[] samples,
-            int samplesLength,
-            short channels,
-            int frequency,
-            float base_quality,
-            int samplesToRead);
-        [DllImport(PLUGIN_NAME,
-            CallingConvention = CallingConvention.Cdecl,
-            EntryPoint = "free_memory_array_for_write_all_pcm_data")]
-        private static extern int FreeMemoryArrayForWriteAllPcmData(
-            System.IntPtr bytesNativeArray);
-
-        [DllImport(PLUGIN_NAME,
-            CallingConvention = CallingConvention.Cdecl,
-            EntryPoint = "read_all_pcm_data_from_file")]
-        private static extern int ReadAllPcmDataFromFile(
-            string filePath,
-            out System.IntPtr samples,
-            out int samplesLength,
-            out short channels,
-            out int frequency,
-            int maxSamplesToRead);
-        [DllImport(PLUGIN_NAME,
-            CallingConvention = CallingConvention.Cdecl,
-            EntryPoint = "read_all_pcm_data_from_memory")]
-        private static extern int ReadAllPcmDataFromMemory(
-            byte[] memoryArray,
-            int memoryArrayLength,
-            out System.IntPtr samples,
-            out int samplesLength,
-            out short channels,
-            out int frequency,
-            int maxSamplesToRead);
-        [DllImport(PLUGIN_NAME,
-            CallingConvention = CallingConvention.Cdecl,
-            EntryPoint = "free_samples_array_native_memory")]
-        private static extern int FreeSamplesArrayNativeMemory(
-            ref System.IntPtr samples);
-
-        [DllImport(PLUGIN_NAME,
-            CallingConvention = CallingConvention.Cdecl,
-            EntryPoint = "open_read_file_stream")]
-        public static extern System.IntPtr OpenReadFileStream(
-            string filePath,
-            out short channels,
-            out int frequency);
-        [DllImport(PLUGIN_NAME,
-            CallingConvention = CallingConvention.Cdecl,
-            EntryPoint = "read_from_file_stream")]
-        public static extern int ReadFromFileStream(
-            System.IntPtr state,
-            float[] samplesToFill,
-            int maxSamplesToRead);
-        [DllImport(PLUGIN_NAME,
-            CallingConvention = CallingConvention.Cdecl,
-            EntryPoint = "close_file_stream")]
-        public static extern int CloseFileStream(
-            System.IntPtr state);
-
-
         public static void Save(
             string filePath,
             UnityEngine.AudioClip audioClip,
@@ -117,7 +34,7 @@ namespace OggVorbis
 
             float[] pcm = new float[audioClip.samples * audioClip.channels];
             audioClip.GetData(pcm, 0);
-            int returnCode = WriteAllPcmDataToFile(filePath, pcm, pcm.Length, finalChannelsCount, audioClip.frequency, quality, samplesToRead);
+            int returnCode = NativeBridge.WriteAllPcmDataToFile(filePath, pcm, pcm.Length, finalChannelsCount, audioClip.frequency, quality, samplesToRead);
             NativeErrorException.ThrowExceptionIfNecessary(returnCode);
         }
 
@@ -135,7 +52,7 @@ namespace OggVorbis
             {
                 throw new System.IO.FileNotFoundException();
             }
-            int returnCode = ReadAllPcmDataFromFile(
+            int returnCode = NativeBridge.ReadAllPcmDataFromFile(
                 filePath,
                 out System.IntPtr pcmPtr,
                 out int pcmLength,
@@ -145,7 +62,7 @@ namespace OggVorbis
             NativeErrorException.ThrowExceptionIfNecessary(returnCode);
             float[] pcm = new float[pcmLength];
             Marshal.Copy(pcmPtr, pcm, 0, pcmLength);
-            FreeSamplesArrayNativeMemory(ref pcmPtr);
+            NativeBridge.FreeSamplesArrayNativeMemory(ref pcmPtr);
             NativeErrorException.ThrowExceptionIfNecessary(returnCode);
 
             var audioClip = UnityEngine.AudioClip.Create(System.IO.Path.GetFileName(filePath), pcmLength / channels, channels, frequency, false);
--- UnityClient/Assets/_Source/Presentation/src/StreamTest.cs
+++ UnityClient/Assets/_Source/Presentation/src/StreamTest.cs
@@ -11,18 +11,19 @@ public class StreamTest : MonoBehaviour
         string workingDirectory = Path.Combine(Application.persistentDataPath, "VorbisPluginTest");
 
         string pathToFile = Path.Combine(workingDirectory, "vivaldi-summer-the-four-seasons.ogg");
-        _fileStream = VorbisPlugin.OpenReadFileStream(pathToFile, out short channels, out int frequency);
-        Debug.Log($"Successesfully opened filestream, channels: {channels}, frequency: {frequency}");
+        //TODO: the VorbisPlugin should implement wrapper class around the file stream int ptr
+        //_fileStream = VorbisPlugin.OpenReadFileStream(pathToFile, out short channels, out int frequency);
+        //Debug.Log($"Successesfully opened filestream, channels: {channels}, frequency: {frequency}");
     }
     private void OnDisable()
     {
-        VorbisPlugin.CloseFileStream(_fileStream);
+        //VorbisPlugin.CloseFileStream(_fileStream);
         Debug.Log($"Successesfully closed filestream");
     }
 
     private void OnAudioFilterRead(float[] data, int channels)
     {
         int requestedSamples = data.Length / channels;
-        VorbisPlugin.ReadFromFileStream(_fileStream, data, requestedSamples);
+        //VorbisPlugin.ReadFromFileStream(_fileStream, data, requestedSamples);
     }
 }
