2b0cef13019b4318cb03a97d18794f9279530983
Konstantin Gindemit <k.hindemit@rambler.ru>
2021-11-03 22:39:31 +0100

    ~ More information when the NativeErrorException is thrown.

--- UnityClient/Assets/VorbisPlugin/Impl/src/NativeErrorException.cs
+++ UnityClient/Assets/VorbisPlugin/Impl/src/NativeErrorException.cs
@@ -5,8 +5,14 @@
         public NativeErrorCode NativeErrorCode { get; }
 
         private NativeErrorException(NativeErrorCode nativeErrorCode)
+            : base($"Error code: {nativeErrorCode}")
         {
             NativeErrorCode = nativeErrorCode;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(NativeErrorException)} {Message}";
         }
 
         internal static void ThrowExceptionIfNecessary(int returnValue)
