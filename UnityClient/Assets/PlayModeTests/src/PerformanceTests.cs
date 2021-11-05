296d985670c406b9c0500b5595c9dbc97e27ed60
Konstantin Gindemit <k.hindemit@rambler.ru>
2021-11-05 22:12:20 +0100

    ~ Added performance test for save function

---
+++ UnityClient/Assets/PlayModeTests/src/PerformanceTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System.IO;
+using Unity.PerformanceTesting;
+using UnityEngine;
+
+namespace PlayModeTests
+{
+    public sealed class PerformanceTests
+    {
+        [Test, Performance]
+        public void TestSavePerformance()
+        {
+            string filesFolder = Path.Combine(Application.persistentDataPath, Consts.TEST_FILE_DIR_NAME);
+            AudioClip sourceAudioClip = Resources.Load<AudioClip>(Consts.SOURCE_AUDIO_CLIP_RESOURCES_PATH_STEREO_48000HZ);
+            string pathToFile = Path.Combine(filesFolder, sourceAudioClip.name) + ".ogg";
+
+
+            Measure.Method(() => { OggVorbis.VorbisPlugin.Save(pathToFile, sourceAudioClip); })
+                .WarmupCount(5)
+                .MeasurementCount(10)
+                .IterationsPerMeasurement(5)
+                .GC()
+                .Run();
+        }
+
+    }
+}
